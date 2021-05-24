2a8142da755a97a68e821b4f280e1663d037f733
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2021-05-24 21:14:02 +0200

    Add button functionality for slides, and setInterval for autochange

--- src/App.js
+++ src/App.js
@@ -6,7 +6,22 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+       setIndex(0);
+    } 
+  }, [index, people]);
 
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [index]);
 
   return (
     <section className='section'>
@@ -36,10 +51,14 @@ function App() {
             </article>
           )
         })}
-        <button className='prev'>
+        <button className='prev' onClick={() => {
+          setIndex(index - 1)
+        }}>
           <FiChevronLeft />
         </button>
-        <button className='next'>
+        <button className='next' onClick={() => {
+          setIndex(index + 1)
+        }}>
           <FiChevronRight />
         </button>
       </div>
